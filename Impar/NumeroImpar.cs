0cff344286e977ae88fa3da9a04818c7fad44eaa
Natassja Lessa <natassjalessa1@gmail.com>
2021-09-14 22:03:56 -0300

    Impar

---
+++ Impar/NumeroImpar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numero_par_ou_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, i, n;
+            Console.Clear();
+            Console.WriteLine("\n\tDigite um número inteiro:");
+            x = int.Parse(Console.ReadLine());
+            for (i = 1; i < x; i++)
+            {
+                n = i%2;
+                if (n%2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
